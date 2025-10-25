44b81ece0666a7cceef4f25f2c95d3d2b85ec599
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2025-10-25 16:37:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "🟡 Running setup commands..."
 
+
 # 1. Run migrations to create the table
 python manage.py migrate --noinput
 
 # 2. Collect static files
 python manage.py collectstatic --noinput
+python manage.py makemigrations api
 
 # 3. Start the Gunicorn server (This must be the final line)
 echo "✅ Setup done. Starting Gunicorn..."
