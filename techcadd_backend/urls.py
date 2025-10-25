984877a57e2168a4b43c47da7e13afe07a6b1e36
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2025-10-25 13:15:07 +0530

    urls

--- techcadd_backend/urls.py
+++ techcadd_backend/urls.py
@@ -1,7 +1,12 @@
-from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
+from django.contrib import admin
+
+def home(request):
+    return HttpResponse("✅ Techcadd Django API is running successfully!")
 
 urlpatterns = [
+    path('', home),  # 👈 add this line
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
