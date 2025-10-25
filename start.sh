dc35be65d7aa32cbb50bd0f220cd234944529405
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2025-10-25 16:27:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "🟡 Running Django migrations..."
 python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python manage.py migrate --noinput && gunicorn techcadd_backend.wsgi --bind 0.0.0.0:$PORT
+# python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 echo "✅ Migrations done, starting Gunicorn..."
 gunicorn techcadd_backend.wsgi:application
